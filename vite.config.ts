9c94451431ea29ac7178145fef4841148138472f
Danil Bogdanchikov <danich282@gmail.com>
2024-11-05 22:22:27 +0300

    feat: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 /// <reference types='vite/client' />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-});
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  console.log("[vite]", process.env.VITE_BASE_URL);
+
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASE_URL || "/",
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+  });
+};
